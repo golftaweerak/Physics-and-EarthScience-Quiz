ff2be5ad71b6025e994285b2187fe3cdc01c61b6
golftaweerak <taweerak.t@promma.ac.th>
2026-08-13 00:59:44 +0700

    Update and deploy web app

--- scripts/profile.js
+++ scripts/profile.js
@@ -378,10 +378,11 @@ function renderUserInfo(game) {
         const user = game.authManager.currentUser;
         if (user && user.email) {
             emailEl.innerHTML = `
-                <svg xmlns="http://www.w3.org/2000/svg" class="h-3.5 w-3.5 inline-block mr-1.5 -mt-0.5 opacity-70" viewBox="0 0 20 20" fill="currentColor">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-3.5 w-3.5 shrink-0 opacity-70" viewBox="0 0 20 20" fill="currentColor">
                     <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z" />
                     <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z" />
-                </svg>${escapeHtml(user.email)}
+                </svg>
+                <span class="truncate max-w-[200px] sm:max-w-[320px] md:max-w-[450px]" title="${escapeHtml(user.email)}">${escapeHtml(user.email)}</span>
             `;
             emailEl.classList.remove('hidden');
         } else {
