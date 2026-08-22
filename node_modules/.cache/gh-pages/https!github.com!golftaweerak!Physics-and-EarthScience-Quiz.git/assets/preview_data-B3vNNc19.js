c39f630baec306a4b966b7d6998fe05ec9b49aea
golftaweerak <taweerak.t@promma.ac.th>
2026-08-22 16:10:35 +0700

    Deploy production dist build

---
+++ node_modules/.cache/gh-pages/https!github.com!golftaweerak!Physics-and-EarthScience-Quiz.git/assets/preview_data-B3vNNc19.js
@@ -0,0 +1,1 @@
+import"./modulepreload-polyfill-B5Qt9EMX.js";/* empty css               */import"./preview-loader-Bq-hfrDl.js";import{G as i}from"./gamification-C8MQJVpH.js";import"./physics_syllabus_data-Mnn3TXVG.js";import"./auth-manager-DxJDYVU6.js";import"./firebase-config-L8WamaTR.js";import"./site-config-I-RQ9TTj.js";import"./gamification-registry-DzpjLHof.js";new i;
